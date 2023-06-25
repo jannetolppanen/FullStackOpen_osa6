1cba56beca68473f201dcf64f7cbcaf98aad65ad
Janne Tolppanen <janne.ks@gmail.com>
2023-06-25 21:32:49 +0300

    6.15 done. new anecdotes are now saved into backend (json-server -- db.json). default votes set to 0 but adding votes in the UI only changes state for now, votes are not yet saved into json-server

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,19 @@
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { createNotification, clearNotification } from '../reducers/notificationReducer'
 import { useDispatch } from 'react-redux'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   // Päästään käsiksi storeen 
   const dispatch = useDispatch()
 
   // Anekdootin lisäys
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    const newAnecdote = await anecdoteService.createNew(content)
     dispatch(createAnecdote(content))  
     dispatch(createNotification(content))
     setTimeout(() => {
